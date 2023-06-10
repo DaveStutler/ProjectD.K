f36e861e5b7983adefa10f1a3fd1c85a497da49c
yukkigu <yukkigu@gmail.com>
2023-06-10 16:39:04 -0700

    Scene transition basic done

--- rip-off-picopark/Assets/Scripts/PlaySceneManager.cs
+++ rip-off-picopark/Assets/Scripts/PlaySceneManager.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+using GeneralSceneManager;
+
 public class PlaySceneManager : MonoBehaviour
 {
-    [SerializeField] private string sceneToLoad;
-    
+    //[SerializeField] private string sceneToLoad;
 
-    // To change scene from Play to Selection screen
-    public void goToSelection()
+    // To change scene from Play to Selection screen.
+    // Build index: 1
+    public void goToSelectionScreen()
     {
-        SceneManager.LoadScene(sceneToLoad);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    // Change scene to Jail Escape scene;
+    // Build index: 2
+    public void goToJailEscape()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Change scene to Dungeon scene;
+    // Build index: 3
+    public void goToDungeon()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    // Change scene to Forest scene;
+    // Build index: 4
+    public void goToForest()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    // Change scene to Island scene;
+    // Build index: 5
+    public void goToIsland()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    // Change scene to Thank you scene;
+    // Build index: 6
+    public void goToThankYou()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
     }
 }
