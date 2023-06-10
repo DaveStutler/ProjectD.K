20b16039f0ebf5a49af58ca5b99fb25095277760
ghaz-zahira <97502500+zahira-ghaz@users.noreply.github.com>
2023-06-10 13:24:06 -0700

    Prefabs altered
    Buttons still need to be done but overall scene setup mostly(?) done.

--- rip-off-picopark/Assets/Scripts/ArrowController.cs
+++ rip-off-picopark/Assets/Scripts/ArrowController.cs
@@ -10,8 +10,15 @@ public class ArrowController : MonoBehaviour
         if (this.gameObject.transform.position.y <= -5)
         {
             Destroy(this.gameObject);
-            Debug.Log("HERE");
         }
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Death")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
